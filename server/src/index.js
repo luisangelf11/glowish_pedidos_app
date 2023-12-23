a7bc5de581befb6b1800a40290000d6c10c98536
luisangelf11 <71238653+luisangelf11@users.noreply.github.com>
2023-12-22 20:56:04 -0400

    statics files configurated

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import {dirname, join} from 'path'
+import { fileURLToPath } from 'url';
+//Imports routes
 import routerCategoria from './routes/categorias.routes.js';
 import routerProductos from './routes/productos.routes.js';
 import routerUsuarios from './routes/usuarios.routes.js';
@@ -10,10 +13,12 @@ import routerCarrito from './routes/carrito.routes.js';
 import routerPedidos from './routes/pedidos.routes.js';
 import routerDetalle from './routes/detalle.routes.js';
 import routerDashboard from './routes/dashboard.routes.js';
+//Proyect middlwares
 import {generateAdminUser} from './middlewares/userAdmin.js';
 
 //Initializations
 const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 //Settings
 app.set('PORT', 3000);
@@ -33,6 +38,9 @@ app.use(routerCarrito);
 app.use(routerPedidos);
 app.use(routerDetalle);
 app.use(routerDashboard);
+
+//Statics files
+app.use(express.static(join(__dirname,'./public')));
 
 //Run Server
 app.listen(app.get('PORT'), ()=>{
