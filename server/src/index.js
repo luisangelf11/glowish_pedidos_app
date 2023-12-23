869e33b02da80906c45e85ccc4d30ca32cee405f
luisangelf11 <71238653+luisangelf11@users.noreply.github.com>
2023-12-22 21:34:53 -0400

    Endpoint for upload files was created (multer was configurated)

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
-import {dirname, join} from 'path'
+import { dirname, extname, join } from 'path'
 import { fileURLToPath } from 'url';
+import multer from 'multer';
 //Imports routes
 import routerCategoria from './routes/categorias.routes.js';
 import routerProductos from './routes/productos.routes.js';
@@ -14,11 +15,29 @@ import routerPedidos from './routes/pedidos.routes.js';
 import routerDetalle from './routes/detalle.routes.js';
 import routerDashboard from './routes/dashboard.routes.js';
 //Proyect middlwares
-import {generateAdminUser} from './middlewares/userAdmin.js';
+import { generateAdminUser } from './middlewares/userAdmin.js';
 
 //Initializations
 const app = express();
 const __dirname = dirname(fileURLToPath(import.meta.url));
+const storage = multer.diskStorage({
+    destination: join(__dirname, 'public/uploads'),
+    filename: (req, file, cb) => {
+        const uniqueId = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, uniqueId + extname(file.originalname).toLocaleLowerCase());
+    }
+});
+
+const upload = multer({
+    storage,
+    fileFilter: (req, file, cb) => {
+        const fileTypes = /jpg|png|jpeg|gif|JPG/;
+        const mimetypes = fileTypes.test(file.mimetype);
+        const extnameFile = fileTypes.test(extname(file.originalname));
+        if (mimetypes && extnameFile) return cb(null, true);
+        cb('Error: el archivo debe ser una imagen valida');
+    }
+});
 
 //Settings
 app.set('PORT', 3000);
@@ -39,11 +58,18 @@ app.use(routerPedidos);
 app.use(routerDetalle);
 app.use(routerDashboard);
 
+//Route for upload files
+app.post('/api/v1/upload', upload.single('file'), (req, res) => {
+    if (!req.file)
+        return res.status(400).json({ "message": 'Please, select a file' });
+    res.json({ "urlImage": `http://localhost:3000/uploads/${req.file.filename}` });
+});
+
 //Statics files
-app.use(express.static(join(__dirname,'./public')));
+app.use(express.static(join(__dirname, './public')));
 
 //Run Server
-app.listen(app.get('PORT'), ()=>{
+app.listen(app.get('PORT'), () => {
     generateAdminUser();
     console.log(`The server running on port ${app.get('PORT')}`);
 });
