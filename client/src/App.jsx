e536fce9874583351a497f920ed7d0b1e710ff34
luisangelf11 <71238653+luisangelf11@users.noreply.github.com>
2024-01-03 20:04:53 -0400

    React Router DOM is config

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,16 @@
+import {Routes, Route} from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import NotFound from './pages/NotFound'
 
 function App() {
   return (
     <>
-     <h1 className="text-center text-cyan-800 p-2 text-3xl">
-      Hello World!
-     </h1>
+     <Routes>
+      <Route path='/' element={<HomePage />}/>
+      <Route path='/login' element={<LoginPage />}/>
+      <Route path='*' element={<NotFound />}/>
+     </Routes>
     </>
   )
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {BrowserRouter as RouterMain} from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterMain>
+      <App />
+    </RouterMain>
   </React.StrictMode>,
 )
